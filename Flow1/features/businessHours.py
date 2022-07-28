8e205b93c0d29518dcefaba70a12845c2a585ad4
AlanSV <alansanchezvallejo@gmail.com>
2022-07-28 12:29:14 -0500

    business hours bug fixed

--- Flow1/features/businessHours.py
+++ Flow1/features/businessHours.py
@@ -11,7 +11,7 @@ def test_businessHours(driver,file):
             EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/div/div[1]/div/div/div[2]/div[2]/div[2]'))
         )
     finally:
-        rand = random.randint(1,2)
+        rand = 2#random.randint(1,2)
         if (rand == 1):
             open24_7 = driver.find_element('xpath','//*[@id="0"]/div[2]/label/span')
             open24_7.click()
@@ -19,7 +19,7 @@ def test_businessHours(driver,file):
             
         elif (rand == 2):
             for i in range(3,10):
-                rand1 = random.randint(1,3)
+                rand1 = 3 #random.randint(1,3)
                 if (rand1 == 1):
                     open24 = driver.find_element('xpath', '//*[@id="0"]/div['+str(i)+']/div[1]/div/div[1]/label/span')
                     open24.click()
@@ -31,41 +31,61 @@ def test_businessHours(driver,file):
                     file.write('    Day: '+ repr(i-2) + ' Closed option was selected' + os.linesep)
 
                 elif (rand1 == 3):
-                    for j in range(1,3):
+                    for j in [1,2]:
                         openTime = driver.find_element('xpath', '//*[@id="0"]/div['+str(i)+']/div[2]/div['+str(j)+']/input')
 
                         if (j == 1):
                             randHour = random.randint(1, 12)
-                            randMin  = random.randint(0, 58)
-                            openTime.send_keys(randHour) 
+                            randMin  = random.randint(0, 59)
+
+                            randHourstr = str(randHour)
+
+                            if(len(randHourstr)<2):
+                                randHourstr = '0'+randHourstr
+                                openTime.send_keys(randHourstr) 
+                            else:
+                                openTime.send_keys(randHour) 
 
                             randMinStr = str(randMin)
                             
-                            if (len(randMinStr)<2):
+                            if (len(randMinStr)<2 ):
+                                randMinStr = '0'+randMinStr
                                 openTime.send_keys(randMinStr)
                                 # randMinStr = '0'+randMinStr
-                                openTime.send_keys(Keys.RIGHT)
-                                randMinStr = '0'+randMinStr
+                                # openTime.send_keys(Keys.RIGHT)
+                                
                             else:
-                                openTime.send_keys(randMinStr)
+                                openTime.send_keys(randMin)
+
                             openTime.send_keys('AM')  
                             file.write('    Day: '+ repr(i-2) + ' Open Time: ' + repr(randHour)+':'+ repr(randMinStr) + 'AM   ')
                             
 
                         elif (j == 2):
                             randHour = random.randint(1, 12)
-                            randMin  = random.randint(0, 58)
-                            openTime.send_keys(randHour) 
+                            randMin  = random.randint(0, 59)
 
+                            randHourstr = str(randHour)
+
+                            if (len(randHourstr)<2):
+                                randHourstr = '0'+randHourstr
+                                openTime.send_keys(randHourstr) 
+                            elif(len(randHourstr)==2):
+                                openTime.send_keys(randHourstr) 
+                                # time.sleep(1)
+
+                             
                             randMinStr = str(randMin)
                             
                             if (len(randMinStr)<2):
+                                randMinStr = '0'+randMinStr
                                 openTime.send_keys(randMinStr)
                                 # randMinStr = '0'+randMinStr
-                                openTime.send_keys(Keys.RIGHT)
-                                randMinStr = '0'+randMinStr
+                                # openTime.send_keys(Keys.RIGHT)
+                                # time.sleep(1)
+                                
                             else:
-                                openTime.send_keys(randMinStr)
+                                openTime.send_keys(randMin)
                             openTime.send_keys('PM') 
                             file.write('    Day: '+repr(i-2)+ ' Close Time: ' + repr(randHour)+':'+ repr(randMinStr) + 'PM' + os.linesep)
                             
